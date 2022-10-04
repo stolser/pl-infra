5d2243ae6b2e6c3325301793e4b9fd2217e6b8e0
Oleh Stoliarov <stoljarov.oleg@gmail.com>
2022-10-04 22:37:44 +0300

    ci: Add Pulumi 'create bucket' workflows (#19)

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
+// The Pulumi program that defines the stack resources
+
 import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+
+// Create a GCS bucket
+const bucketArgs = {
+    location: "EU",
+    storageClass: "STANDARD"
+}
+const bucket = new gcp.storage.Bucket("test-bucket", bucketArgs);
+
+// Export the base URL of the bucket, in the format gs://<bucket-name>
+export const bucketName = bucket.url;
