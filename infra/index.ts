f2f0862250a5917c5fa4462282feb6c7aeb8031c
Oleh Stoliarov <stoljarov.oleg@gmail.com>
2022-10-04 23:44:32 +0300

    ci(infra): Create Pub/Sub topic and push sub

--- infra/index.ts
+++ infra/index.ts
@@ -12,3 +12,24 @@ const bucket = new gcp.storage.Bucket("test-bucket", bucketArgs);
 
 // Export the base URL of the bucket, in the format gs://<bucket-name>
 export const bucketName = bucket.url;
+
+const psTopicPulumiTest = new gcp.pubsub.Topic("pulumi-test", {
+    labels: {
+        created: "pulumi",
+    },
+    messageRetentionDuration: "86600s",
+});
+
+const exampleSubscription = new gcp.pubsub.Subscription("pulumi-test-sub-push", {
+    topic: psTopicPulumiTest.name,
+    ackDeadlineSeconds: 20,
+    labels: {
+        created: "pulumi",
+    },
+    pushConfig: {
+        pushEndpoint: "https://example.com/push",
+        attributes: {
+            "x-goog-version": "v1",
+        },
+    },
+});
