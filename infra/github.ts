2347c6caa4f222f47bf03f00efa4a58ffb5ec7cb
Oleh Stoliarov <stoljarov.oleg@gmail.com>
2022-10-14 23:40:00 +0300

    feat: Add branch protection to 'graphql-book-list' (#45)

--- infra/github.ts
+++ infra/github.ts
@@ -1,50 +1,40 @@
-import * as github from "@pulumi/github";
-import {BranchDefault, BranchProtection} from "@pulumi/github";
-
-let mainBranchName = "master";
+import {Repository} from "@pulumi/github";
+import * as github from "./utils/github-utils"
 
 export function createGitHubRepos() {
 
-    function newPublicRepository(repoName: string,
-                                 repoDescription: string,
-                                 repoTopics: Array<string>) {
+    /**
+     * Create repo 'github-actions'
+     */
+    function createGitHubActionsRepo() {
+        const githubActionsRepo: Repository = github.newPublicRepository(
+            "github-actions",
+            "Monorepo for custom GitHub Actions",
+            ["node", "monorepo", "github-actions"]
+        );
 
-        return new github.Repository(repoName, {
-            name: repoName,
-            description: repoDescription,
-            visibility: "public",
-            topics: repoTopics,
-            allowSquashMerge: true,
-            allowRebaseMerge: false,
-            allowMergeCommit: false,
-            allowAutoMerge: false,
-            deleteBranchOnMerge: true
-        });
+        github.createDefaultBranch("ga-repo-default-branch", githubActionsRepo)
+        github.createBranchProtection("ga-repo-branch-protection", githubActionsRepo)
     }
 
-    const githubActionsRepo = newPublicRepository(
-        "github-actions",
-        "Monorepo for custom GitHub Actions",
-        ["node", "monorepo", "github-actions"]
-    );
+    /**
+     * Create repo 'graphql-book-list'
+     */
+    function createGraphQlBookListRepo() {
+        const graphQlRepo = github.newPublicRepository(
+            "graphql-book-list",
+            "A simple web app built with Node.js, GraphQL, Express, MongoDB, React, and Apollo",
+            ["node", "graphql", "express", "mongodb", "react", "apollo"]
+        );
 
-    new BranchDefault("ga-repo-default-branch", {
-        branch: mainBranchName,
-        repository: githubActionsRepo.name
-    });
+        github.createDefaultBranch("book-list-repo-default-branch", graphQlRepo)
+        github.createBranchProtection("book-list-repo-branch-protection", graphQlRepo)
+    }
 
-    new BranchProtection("ga-repo-branch-protection", {
-        pattern: mainBranchName,
-        repositoryId: githubActionsRepo.name,
-        allowsForcePushes: false,
-        enforceAdmins: true,
-        requireConversationResolution: true,
-        requireSignedCommits: true
-    })
 
-    const graphQlRepo = newPublicRepository(
-        "graphql-book-list",
-        "A simple web app built with Node.js, GraphQL, Express, MongoDB, React, and Apollo",
-        ["node", "graphql", "express", "mongodb", "react", "apollo"]
-    );
+    // Create GitHub repositories
+
+    createGitHubActionsRepo();
+
+    createGraphQlBookListRepo();
 }
---
+++ infra/utils/github-utils.ts
@@ -0,0 +1,44 @@
+import * as github from "@pulumi/github";
+import {BranchDefault, BranchProtection, Repository} from "@pulumi/github";
+
+let mainBranchName = "master";
+
+export function newPublicRepository(repoName: string,
+                                    repoDescription: string,
+                                    repoTopics: Array<string>) {
+
+    return new github.Repository(repoName, {
+        name: repoName,
+        description: repoDescription,
+        visibility: "public",
+        topics: repoTopics,
+        allowSquashMerge: true,
+        allowRebaseMerge: false,
+        allowMergeCommit: false,
+        allowAutoMerge: false,
+        deleteBranchOnMerge: true
+    });
+}
+
+export function createDefaultBranch(resourceName: string,
+                                    repo: Repository) {
+
+    new BranchDefault(resourceName, {
+        branch: mainBranchName,
+        repository: repo.name
+    });
+}
+
+export function createBranchProtection(resourceName: string,
+                                       repo: Repository) {
+
+    new BranchProtection(resourceName, {
+        pattern: mainBranchName,
+        repositoryId: repo.name,
+        allowsForcePushes: false,
+        enforceAdmins: true,
+        requireConversationResolution: true,
+        requireSignedCommits: true,
+        requiredLinearHistory: true
+    })
+}
