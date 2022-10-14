ada6996be58827662459c3e13c2acad1de5ca3d0
Oleh Stoliarov <stoljarov.oleg@gmail.com>
2022-10-14 22:53:19 +0300

    feat: Create repo 'graphql-book-list' (#44)

--- infra/github.ts
+++ infra/github.ts
@@ -4,16 +4,29 @@ import {BranchDefault, BranchProtection} from "@pulumi/github";
 let mainBranchName = "master";
 
 export function createGitHubRepos() {
-    const githubActionsRepo = new github.Repository("github-actions", {
-        name: "github-actions",
-        description: "Monorepo for custom GitHub Actions",
-        visibility: "public",
-        topics: ["monorepo", "github-actions"],
-        allowSquashMerge: true,
-        allowRebaseMerge: false,
-        allowMergeCommit: false,
-        allowAutoMerge: false
-    });
+
+    function newPublicRepository(repoName: string,
+                                 repoDescription: string,
+                                 repoTopics: Array<string>) {
+
+        return new github.Repository(repoName, {
+            name: repoName,
+            description: repoDescription,
+            visibility: "public",
+            topics: repoTopics,
+            allowSquashMerge: true,
+            allowRebaseMerge: false,
+            allowMergeCommit: false,
+            allowAutoMerge: false,
+            deleteBranchOnMerge: true
+        });
+    }
+
+    const githubActionsRepo = newPublicRepository(
+        "github-actions",
+        "Monorepo for custom GitHub Actions",
+        ["node", "monorepo", "github-actions"]
+    );
 
     new BranchDefault("ga-repo-default-branch", {
         branch: mainBranchName,
@@ -28,4 +41,10 @@ export function createGitHubRepos() {
         requireConversationResolution: true,
         requireSignedCommits: true
     })
+
+    const graphQlRepo = newPublicRepository(
+        "graphql-book-list",
+        "A simple web app built with Node.js, GraphQL, Express, MongoDB, React, and Apollo",
+        ["node", "graphql", "express", "mongodb", "react", "apollo"]
+    );
 }
